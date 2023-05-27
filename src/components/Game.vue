a23e5344883eee4504fe203efe35b28e5b1bc076
Nicholas Peters <peters.nicholas@gmail.com>
2023-05-27 16:54:47 +0200

    get players

---
+++ src/components/Game.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import { IPlayer } from '../models/Player';
+import Login from './Login.vue';
+
+const gameIsRunning = ref<boolean>(false);
+const playerX = ref<IPlayer>({ name: '', symbol: 'X', score: 0 });
+const playerO = ref<IPlayer>({ name: '', symbol: 'O', score: 0 });
+
+
+
+function handleGetPlayers(playerXname: string, playerOname: string) {
+    playerX.value.name = playerXname;
+    playerO.value.name = playerOname;
+}
+
+</script>
+
+<template>
+    <Login v-if="!gameIsRunning" @getPlayers="handleGetPlayers" />
+</template>
+
+<style scoped></style>
---
+++ src/components/Login.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const emits = defineEmits<{(e: 'getPlayers', nameX: string, nameO: string): void}>()
+
+function getPlayers() {
+    emits("getPlayers", playerXname.value, playerOname.value);   
+}
+
+const playerXname = ref<string>('');
+const playerOname = ref<string>('');
+
+</script>
+
+<template>
+    <input type="text" v-model="playerXname" placeholder="Player Xs Name">
+    <input type="text" v-model="playerOname" placeholder="Player Os Name">
+    <button @click="getPlayers">Start Game</button>
+</template>
+
+<style scoped></style>

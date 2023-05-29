57ac1931b0154c3359c97f63b2f08223a165823f
Nicholas Peters <peters.nicholas@gmail.com>
2023-05-29 11:12:11 +0200

    get current player, and render grid of squares

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -2,22 +2,37 @@
 import { ref } from 'vue';
 import { IPlayer } from '../models/Player';
 import Login from './Login.vue';
+import GameBoard from './GameBoard.vue';
 
-const gameIsRunning = ref<boolean>(false);
-const playerX = ref<IPlayer>({ name: '', symbol: 'X', score: 0 });
-const playerO = ref<IPlayer>({ name: '', symbol: 'O', score: 0 });
+let gameIsRunning = ref<boolean>(false);
+let playerXturn = ref<boolean>(true);
+let currentPlayer = ref<IPlayer>({} as IPlayer);
 
+let playerX = ref<IPlayer>({ name: '', symbol: 'X', score: 0 });
+let playerO = ref<IPlayer>({ name: '', symbol: 'O', score: 0 });
 
+if(playerXturn.value) {
+    currentPlayer.value = playerX.value;
+    console.log(currentPlayer.value);
+    
+} else {
+    currentPlayer.value = playerO.value;
+    console.log(currentPlayer.value);
+}
 
 function handleGetPlayers(playerXname: string, playerOname: string) {
     playerX.value.name = playerXname;
     playerO.value.name = playerOname;
+    gameIsRunning.value = true;
+    console.log(playerX.value, playerO.value);
+    
 }
 
 </script>
 
 <template>
     <Login v-if="!gameIsRunning" @getPlayers="handleGetPlayers" />
+    <GameBoard v-else :currentPlayer="currentPlayer" />
 </template>
 
 <style scoped></style>
---
+++ src/components/GameBoard.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { IPlayer } from '../models/Player';
+import { ref } from 'vue';
+
+const props = defineProps<{currentPlayer: IPlayer}>();
+
+defineEmits<{(e: 'handleClick'): void}>()
+
+const squares = ref<string[]>(['', '', '', '', '', '', '', '', '']);
+
+</script>
+
+<template>
+    <div class="grid">
+        <div class="square" v-for="(square, i) in squares">{{ i }}</div>
+    </div>
+</template>
+
+<style scoped>
+
+    .grid {
+        display: grid;
+        grid-template-columns: repeat(3, 1fr);
+        width: 150px;
+        height: 150px;
+        background-color: rgb(0, 0, 0);
+        margin: 0 auto;
+    }
+
+    .square {
+        width: 50px;
+        height: 50px;
+        border: 1px solid rgb(255, 255, 255);
+    }
+
+</style>

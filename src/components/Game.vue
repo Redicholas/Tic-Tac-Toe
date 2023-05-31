278faa9fd3da1e3e7e2bf8f2a0231d25b97970ba
Nicholas Peters <peters.nicholas@gmail.com>
2023-05-31 16:17:35 +0200

    add stats and reset game functionality

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -4,6 +4,7 @@ import { IPlayer } from '../models/Player';
 import Login from './Login.vue';
 import GameBoard from './GameBoard.vue';
 
+
 let gameIsRunning = ref<boolean>(false);
 
 let playerX = ref<IPlayer>({ name: '', symbol: 'X', score: 0 });
@@ -16,10 +17,16 @@ function handleGetPlayers(playerXname: string, playerOname: string) {
     // console.log(playerX.value, playerO.value);
 }
 
+function reset() {
+    gameIsRunning.value = false;
+    playerX.value = { name: '', symbol: 'X', score: 0 };
+    playerO.value = { name: '', symbol: 'O', score: 0 };
+}
+
 </script>
 
 <template>
-    <GameBoard v-if="gameIsRunning" :playerX="playerX" :playerO="playerO" />
+    <GameBoard v-if="gameIsRunning" :playerX="playerX" :playerO="playerO" @reset="reset" />
     <Login v-else @getPlayers="handleGetPlayers" />
 </template>
 
--- src/components/GameBoard.vue
+++ src/components/GameBoard.vue
@@ -3,20 +3,28 @@ import { IPlayer } from '../models/Player';
 import { ref } from 'vue';
 
 const props = defineProps<{currentPlayer: IPlayer, playerX: IPlayer, playerO: IPlayer}>();
+const emits = defineEmits<{(e: 'reset', empty: string): void}>();
 const squares = ref<string[]>(['', '', '', '', '', '', '', '', '']);
 
 let playerXturn = ref<boolean>(true);
 let currentPlayer = ref<IPlayer>(props.playerX as IPlayer);
 
 function handleClick(i: number) {
-  currentPlayer.value = playerXturn.value ? props.playerX : props.playerO;
-  squares.value[i] = currentPlayer.value.symbol;
-  if (weHaveAWinner()) {
-    alert(`${currentPlayer.value.name} wins!`);
-    return;
-  }
-  playerXturn.value = !playerXturn.value;
-  currentPlayer.value = playerXturn.value ? props.playerX : props.playerO;
+    if (squares.value[i] != '') return
+    if (!weHaveAWinner()) {
+        squares.value[i] = currentPlayer.value.symbol;
+    } else {
+        return;
+    }
+    if (gameIsEven()) {
+        alert(`The game is even, play again!`)
+    }
+    if (weHaveAWinner()) {
+        currentPlayer.value.score += 1;
+        return;
+    }
+    playerXturn.value = !playerXturn.value;
+    currentPlayer.value = playerXturn.value ? props.playerX : props.playerO;
 }
 
 function weHaveAWinner() {
@@ -43,46 +51,79 @@ function weHaveAWinner() {
     return false;
 }
 
-function resetGame() {
+function gameIsEven() {
+    return squares.value.every(square => square !== '') && !weHaveAWinner();
+}
+
+function playAgain() {
     squares.value = ['', '', '', '', '', '', '', '', ''];
     playerXturn.value = true;
     currentPlayer.value = props.playerX as IPlayer;
 }
 
+function reset() {
+    squares.value = ['', '', '', '', '', '', '', '', ''];
+    emits('reset', '');
+}
+
 </script>
 
 <template>
-    <h2 v-if="weHaveAWinner()">{{ currentPlayer.name }} won the game! <br />Reset to play again</h2>
+    <h2 v-if="weHaveAWinner()">{{ currentPlayer.name }} won the game! 
+        <br />Play again</h2>
+    <h2 v-else-if="gameIsEven()">The game is even, play again!</h2>
     <h2 v-else >{{ currentPlayer.name }}'s Turn</h2>
-    <div class="grid">
-        <div class="square" v-for="(square, i) in squares"
-             :key="i"
-             @click="() => handleClick(i)">
-             {{ square }}
+    <main>
+        <div class="grid">
+            <div class="square" v-for="(square, i) in squares"
+            :key="i"
+            @click="() => handleClick(i)">
+            {{ square }}
         </div>
-    </div>
-    <button @click="resetGame">Reset</button>
+        </div>
+        <div class="stats">
+            <h3>Score</h3>
+            <p>{{ props.playerX.name }}: {{ props.playerX.score }}</p>
+            <p>{{ props.playerO.name }}: {{ props.playerO.score }}</p>
+            <button @click="playAgain">Play Again</button>
+            <button @click="reset">Reset</button>
+        </div>
+    </main>
 </template>
 
 <style scoped>
 
+    main {
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+        gap: 2rem;
+    }
+
+    .stats {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        gap: 5px;
+    }
     .grid {
         display: grid;
         grid-template-columns: repeat(3, 1fr);
-        width: 150px;
-        height: 150px;
+        width: 15rem;
+        height: 15rem;
         background-color: rgb(0, 0, 0);
         margin: 1rem auto;
     }
 
     .square {
-        width: 50px;
-        height: 50px;
+        width: 5rem;
+        height: 5rem;
         border: 1px solid rgb(255, 255, 255);
         display: flex;
         justify-content: center;
         align-items: center;
-        font-size: 30px;
+        font-size: 5rem;
     }
 
 </style>
